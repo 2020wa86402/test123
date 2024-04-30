30b914c0dcf717d17cdf09b41e650cd2cc79203d
Wims2024 <ayushi.mishra@takeda.com>
2024-04-30 19:45:09 +0530

    adding same function from main branch

--- file_1.c
+++ file_1.c
@@ -8,7 +8,7 @@ int main() {
  
     // calculate the sum
     sum = number1 + number2;
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d - %d = %d", number1, number2, sum);
     return 0;
 }
 //function added in branch1
@@ -17,5 +17,5 @@ int func_add(int c, int d)
         // i am in main branch
         // i am making this changes in
         // one more change
-        return c+d;
+        return c-d;
 }
